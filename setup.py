afc68af13e57beab86dc1c926d7f23850fe490d0
amoghjalihal <amogh_jalihal@hms.harvard.edu>
2025-01-13 19:55:14 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='QLD',
+      version='0.1',
+      description="quantitative limiting dilutions",
+      #url='http://github.com/storborg/funniest',
+      author='Amogh Jalihal',
+      author_email='amogh_jalihal@hms.harvard.edu',
+      license='MIT',
+      packages=['QLD'],
+      zip_safe=False)
